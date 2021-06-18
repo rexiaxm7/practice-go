4251f609abb1b02180bc244a19790ac5420b5ede
rexiaxm7 <rexiaxm7@gmail.com>
2021-06-19 06:54:02 +0900

    feat: mysqlをdocker-composeで構築できるように変更

---
+++ docker/db/sql/99-sample-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO practice.user (name) VALUES ('小林 正明');
+INSERT INTO practice.user (name) VALUES ('加藤 真吾');
+INSERT INTO practice.user (name) VALUES ('今井 徹');
+INSERT INTO practice.user (name) VALUES ('清水 隆之');
+INSERT INTO practice.user (name) VALUES ('和田 裕之');
+INSERT INTO practice.user (name) VALUES ('近藤 彰');
+INSERT INTO practice.user (name) VALUES ('田中 昌幸');
